13782ea4cd9b361558183628b081d9358b9f0561
unknown <einsenhorn@gmail.com>
2014-01-18 03:26:07 +0100

    Modification mineure

--- app/src/AdminModule/Controller/UserController.js
+++ app/src/AdminModule/Controller/UserController.js
@@ -1,16 +1,12 @@
 angular.module('AdminModule').controller('UserController', ['$scope', '$rootScope', '$routeParams', '$location', 'Admin', function($scope, $rootScope, $routeParams, $location, Admin) {
     $scope.users = [];
     $scope.error = "";
-/*
+
     Admin.r.getUsers(function(data) {
             $scope.users = data.users;
         }, function() {
             $scope.error = "An error has occurred !";
     });
-*/
-    //mock up
-    $scope.users = [{ login: 'toto', space: 4242 }, { login: 'titi', space: 1231232131  }, { login: 'tutu', space: 21  }, { login: 'tata', space: 0  }];
-    //mock up
 
     $scope.deleteUser = function(user, index) {
         Admin.r.deleteUser({ user: user.id }, {}, function() {
